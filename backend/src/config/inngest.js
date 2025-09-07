00db7bbad9a38f728f5742904597c33d2afa771d
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-09-07 21:24:38 +0700

    stream & sentry added

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/user.model.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "slack-clone" });
 
@@ -20,6 +21,12 @@ const syncUser = inngest.createFunction(
         };
 
         await User.create(newUser);
+
+        await upsertStreamUser({
+            id: newUser.clerkId.toString(),
+            name: newUser.name,
+            image: newUser.image,
+        });
     }
 );
 
@@ -30,6 +37,8 @@ const deleteUserFromDB = inngest.createFunction(
         await connectDB();
         const { id } = event.data;
         await User.deleteOne({ clerkId: id });
+
+        await deleteStreamUser(id.toString());
     }
 );
 
